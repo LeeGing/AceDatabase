ce6d95bcafda70ab0a16ae6dcf1803211948b84a
Alex Hache <ahache8@gmail.com>
2017-08-19 20:01:48 +0000

    Fixed migration and seed file

--- db/migrations/20170817015432_create_items.js
+++ db/migrations/20170817015432_create_items.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("items", (table) => {
-    table.integer('id');
+    table.increments('id');
     table.string('name');
     table.integer('price');
   });
--- db/seeds/items.js
+++ db/seeds/items.js
@@ -2,21 +2,21 @@ exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(function () {
       return Promise.all([
-        knex('items').insert({id: 1, name: 'Avalanche Crunch', price: 9}),
-        knex('items').insert({id: 2, name: 'Banana Cream', price: 9}),
-        knex('items').insert({id: 3, name: 'Chocolate Banana', price: 9}),
-        knex('items').insert({id: 4, name: 'Chocolate Crunch', price: 9}),
-        knex('items').insert({id: 5, name: 'Peanut Butter', price: 9}),
-        knex('items').insert({id: 6, name: 'Red Devil', price: 9}),
-        knex('items').insert({id: 7, name: 'Samoa', price: 9}),
-        knex('items').insert({id: 8, name: 'Smore', price: 9}),
-        knex('items').insert({id: 9, name: 'Snickers', price: 9}),
-        knex('items').insert({id: 10, name: 'Strawberry', price: 9}),
-        knex('items').insert({id: 11, name: 'Strawberry Ice', price: 9}),
-        knex('items').insert({id: 12, name: 'Twix Chocolate', price: 9}),
-        knex('items').insert({id: 13, name: 'Drink 1', price: 8}),
-        knex('items').insert({id: 14, name: 'Drink 2', price: 8}),
-        knex('items').insert({id: 15, name: 'Drink 3', price: 8})
+        knex('items').insert({name: 'Avalanche Crunch', price: 9}),
+        knex('items').insert({name: 'Banana Cream', price: 9}),
+        knex('items').insert({name: 'Chocolate Banana', price: 9}),
+        knex('items').insert({name: 'Chocolate Crunch', price: 9}),
+        knex('items').insert({name: 'Peanut Butter', price: 9}),
+        knex('items').insert({name: 'Red Devil', price: 9}),
+        knex('items').insert({name: 'Samoa', price: 9}),
+        knex('items').insert({name: 'Smore', price: 9}),
+        knex('items').insert({name: 'Snickers', price: 9}),
+        knex('items').insert({name: 'Strawberry', price: 9}),
+        knex('items').insert({name: 'Strawberry Ice', price: 9}),
+        knex('items').insert({name: 'Twix Chocolate', price: 9}),
+        knex('items').insert({name: 'Horchata', price: 12}),
+        knex('items').insert({name: 'Strawchata', price: 12}),
+        knex('items').insert({name: 'Coconut Horchata', price: 12})
       ])
     });
 };
